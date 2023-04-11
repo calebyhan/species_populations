4c046deb8865894e09d2f8bc56dd702406929ef3
Caleb Han <calebhantech@gmail.com>
2023-04-11 09:58:12 -0400

    commit

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import json
 import matplotlib.pyplot as plt
 import math
+import scipy.stats
 
 
 def get_data():
@@ -21,7 +22,7 @@ def get_data():
 
 
 def plots():
-    with open("data/data.json", encoding="utf-8") as f:
+    with open("data/data.json", "r", encoding="utf-8") as f:
         data = json.load(f)
     for i in data:
         x = []
@@ -40,4 +41,39 @@ def plots():
         plt.clf()
 
 
-plots()
+def trends():
+    results = {"positive": 0, "negative": 0, "inconclusive": 0, "data": []}
+    with open("data/data.json", "r", encoding="utf-8") as f:
+        data = json.load(f)
+    for i in data:
+        x = []
+        y = []
+        for index, count in enumerate(data[i]["counts"]):
+            if not math.isnan(count):
+                x.append(index + 1950)
+                y.append(count)
+        if x != [] and y != []:
+            slope, intercept, r, p, std_err = scipy.stats.linregress(x, y)
+            results["data"].append([slope, intercept, r, p, std_err])
+            if slope > 0:
+                results["positive"] += 1
+            elif slope < 0:
+                results["negative"] += 1
+        else:
+            results["inconclusive"] += 1
+    with open("data/results.json", "w", encoding="utf-8") as f:
+        json.dump(results, f, indent=4)
+
+
+def analysis():
+    with open("data/results.json", "r", encoding="utf-8") as f:
+        results = json.load(f)
+    total = results["positive"] + results["negative"]
+    plt.bar(["Positive", "Negative"], [results["positive"] / total, results["negative"] / total])
+    plt.xlabel("Correlation")
+    plt.ylabel("Percentage")
+    plt.title("Overall results")
+    plt.show()
+
+
+analysis()
